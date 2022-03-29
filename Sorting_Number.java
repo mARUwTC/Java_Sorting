7b3086bb2e60d08f0a0fdb6d7cc03e2e5da55159
mARUwTC <86964487+mARUwTC@users.noreply.github.com>
2022-03-29 17:16:32 +0800

    Add files via upload

---
+++ Sorting_Number.java
@@ -0,0 +1,31 @@
+import java.io.*;
+class S20170182 {
+    public static void main(String[] args) 
+    throws IOException {
+        InputStreamReader reader = new InputStreamReader(System.in);
+        BufferedReader buf = new BufferedReader(reader);
+
+		System.out.println("Please enter 3 integers.\nThis program will sort them in assending order.\nEnter the 1st integer: ");
+        String a = buf.readLine();
+        int x = Integer.parseInt(a);
+        System.out.println("Enter the 2nd integer: ");
+        String b = buf.readLine();
+        int y = Integer.parseInt(b);
+        System.out.println("Enter the 3rd integer: ");
+        String c = buf.readLine();
+        int z = Integer.parseInt(c);
+
+        if ((x > y) && (y > z))
+            System.out.println("The smallest value is: " + z + "\nThe 2nd smallest value is: " + y + "\nThe largest value is: " + x);
+        else if ((x < y) && (y < z))
+            System.out.println("The smallest value is: " + x + "\nThe 2nd smallest value is: " + y + "\nThe largest value is: " + z);
+        else if ((x < y) && (z < y) && (x < z))
+            System.out.println("The smallest value is: " + x + "\nThe 2nd smallest value is: " + z + "\nThe largest value is: " + y);
+        else if ((x < y) && (z < y) && (x > z))
+            System.out.println("The smallest value is: " + z + "\nThe 2nd smallest value is: " + x + "\nThe largest value is: " + y);
+        else if ((x > y) && (z > y) && (x > z))
+            System.out.println("The smallest value is: " + y + "\nThe 2nd smallest value is: " + z + "\nThe largest value is: " + x);
+        else if ((x > y) && (z > y) && (x < z))
+            System.out.println("The smallest value is: " + y + "\nThe 2nd smallest value is: " + x + "\nThe largest value is: " +z);
+    }
+}
